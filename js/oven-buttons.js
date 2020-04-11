03b382bf44bab7c0fe9fc2e6687f07ee7499ac79
Kara Gates <karagates@google.com>
2020-04-11 13:45:56 -0700

    Oven open close  / counter
    Created temperature up/down controls (unstyled)
    added fonts

---
+++ js/oven-buttons.js
@@ -0,0 +1,22 @@
+let temperature = document.getElementById('temperature');
+
+    let temp = 0;
+    function tempUp() {
+        temp += 10;
+        document.getElementById("temperature").innerHTML = temp;
+    };
+
+    function tempDown() {
+        if (temp > 1) {
+        temp -= 10;
+        }
+        document.getElementById("temperature").innerHTML = temp;
+    };
+
+
+var element = document.querySelector(".door");
+element.addEventListener("click", toggleDoor);
+
+function toggleDoor() {
+  element.classList.toggle("doorOpen");
+}
